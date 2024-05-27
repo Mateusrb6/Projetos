2d449968d1ebac647129f4b3a5f262e9556da73b
mateusrb6 <241008513@aluno.unb.br>
2024-05-27 18:03:12 -0300

    add

--- CalculadoraSimples.py
+++ CalculadoraSimples.py
@@ -1,53 +1,56 @@
 # calculadora simples com operacoes basicas
 
-def menu():
+def menuInicial():
     print('\tCalculadora simples')
-    print('Escolha uma operacao: \n')
-    print('1) Soma\n2) Subtracao\n3) Multiplicacao\n4)Divisao\n5) Exponenciacao')
+    print('Escolha uma operacao: ')
+    print('1) Soma\n2) Subtracao\n3) Multiplicacao\n4) Divisao\n5) Exponenciacao')
 
 def soma():
     n1 = float(input('n1 = '))
     n2 = float(input('n2 = '))
     sum = n1 + n2
-    return sum
+    print(f'{n1} + {n2} = {sum}')
 
 def sub():
     n1 = float(input('n1 = '))
     n2 = float(input('n2 = '))
     subtracao = n1 - n2
-    return subtracao
+    print(f'{n1} - {n2} = {subtracao}')
 
 def mult():
     n1 = float(input('n1 = '))
     n2 = float(input('n2 = '))
     multiplicacao = n1 * n2
-    return multiplicacao
+    print(f'{n1} * {n2} = {multiplicacao}')
 
 def divisao():
     n1 = float(input('n1 = '))
     n2 = float(input('n2 = '))
     divi = n1 / n2
-    return divi
+    print(f'{n1} / {n2} = {divi}')
+
+def expo():
+    n1 = float(input('n1 = '))
+    n2 = float(input('n2 = '))
+    exponencicacao = n1 ** n2
+    print(f'{n1} ^ {n2} = {exponencicacao}')
 def optionsMenu():
     options = int(input('operacao = '))
-    while True:
-        match options:
-            case 1:
-                soma()
-            case 2:
-                sub()
-            case 3:
-                mult()
-            case 4:
-                divisao()
+
+    match options:
+        case 1:
+            soma()
+        case 2:
+            sub()
+        case 3:
+            mult()
+        case 4:
+            divisao()
+        case 5:
+            expo()
 
 
-
-
-
-
-
-menu()
+menuInicial()
 optionsMenu()
 
 
